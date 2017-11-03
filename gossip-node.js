19623d40ca203677ef9ad6c7511717f3e6edb3d2
Cache Staheli <cache@byu.edu>
2017-11-03 17:07:11 -0600

    Flesh outs some of the APIs

--- gossip-node.js
+++ gossip-node.js
@@ -1,8 +1,12 @@
 const uuid = require('uuid/v4');
+var request = require('request');
 
 function GossipNode(input, done) {
-    this.nodeId = input.nodeId;
     var self = this;
+    this.nodeId = input.nodeId;
+    this.url = input.url;
+    this.nodeUrl = input.url + "/node/" + self.nodeId;
+    this.state = {};
 
     this.generateUuid = function () {
         return uuid();
@@ -33,7 +37,7 @@ function GossipNode(input, done) {
 
     /**
      * Generates a Want message with the form:
-     *
+     * <pre>
      *   {"Want": ["ABCD-1234-ABCD-1234-ABCD-125A": 3,
      *             "ABCD-1234-ABCD-1234-ABCD-129B": 5,
      *             "ABCD-1234-ABCD-1234-ABCD-123C": 10,
@@ -41,11 +45,22 @@ function GossipNode(input, done) {
      *            ] ,
      *    "EndPoint": "https://example.com/gossip/asff3"
      *   }
-     * @param {Object[]} originIds  an array of pairs, of the form.
+     * </pre>
+     * This expects <code>originIds</code> to be in the form:
+     *  <pre>
+     * [
+     *      {
+     *          "uuid": "ABCD-1234-ABCD-1234-ABCD-125A",
+     *          "seqNo": 3
+     *      },
+     *      ...
+     * ]
+     * </pre>
+     * @param {Object[]} originIds  an array of pairs
      * @param {string} originIds[].uuid the id of the pair.
-     * @param {string} originIds[].seqNo the sequence number associated with the id.
+     * @param {number} originIds[].seqNo the sequence number associated with the id.
      * @param endPoint URL of the node propagating the rumor.
-     * @returns {{Want: Array, EndPoint: *}}
+     * @returns {{Want: Array, EndPoint: String}}
      */
     this.generateWantMessage = function (originIds, endPoint) {
         var wantMessage = {
@@ -57,6 +72,69 @@ function GossipNode(input, done) {
         }
         return wantMessage;
     };
+
+    function getRandomArrayIndex(length) {
+        return Math.floor(Math.random() * (length - 1));
+    }
+
+    function getPeer(state, callback) {
+        request.get(self.url + "peers/" + self.nodeId, function(err, res, body) {
+            if (!err && body) {
+                var peers = JSON.parse(body);
+                var length = Object.keys(peers).length;
+                if (length === 0) {
+                    callback(null);
+                } else if (length === 1) {
+                    callback(peers[0]);
+                } else {
+                    return peers[getRandomArrayIndex(length)];
+                }
+            } else {
+                callback(null);
+            }
+        });
+    }
+
+    function chooseToSendRumor(){
+        return (Math.random() > 0.5);
+    }
+
+    function prepareMessage(state, peer) {
+        if (chooseToSendRumor()) {
+            self.generateRumorMessage(null, null, null, null);
+        }
+        else {
+            self.generateWantMessage(null, null);
+        }
+    }
+
+    function sendMessage(url, message) {
+        request.post(
+            url,
+            {
+                json: {
+                    message: message
+                }
+            },
+            function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    console.log(body)
+                }
+            }
+        );
+    }
+
+    while (true) {
+        setTimeout(function () {
+            getPeer(self.state, function (peer) {
+                if (peer !== null) {
+                    var message = prepareMessage(self.state, peer);
+                    var url = peer.nodeUrl;
+                    sendMessage(url, message);
+                }
+            });
+        }, 200);
+    }
 }
 
 module.exports = GossipNode;
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,10 @@ var router = express.Router();
 const rn = require('random-number');
 const spawn = require('threads').spawn;
 
-var GossipNode = require('gossip-node');
+var GossipNode = require('../gossip-node');
 var Message = require('../model/message');
 var Relationship = require('../model/relationship');
 var Rumor = require('../model/rumor');
-var Want = require('../model/want');
 
 var gen = rn.generator({
     min: 0
@@ -24,31 +23,124 @@ router.get('/', function (req, res, next) {
 
 router.get('/new/nodeId', function (req, res) {
     session = req.session;
-    res.json(gen);
+    var id = gen;
+    console.log(id);
+    res.json(id);
+});
+
+/**
+ * Gets the rumors from a node for a specific message, greater than the given sequence number.
+ * @param nodeId the node to get the rumors for.
+ * @param messageId the message Id to get messages for.
+ * @param seqNo the sequence number to look past. Gets messages with sequences greater than this.
+ * @return {Promise} a promise from the database <code>exec()</code>
+ */
+function getRumors(nodeId, messageId, seqNo) {
+    return Rumor.find({
+        nodeId: nodeId,
+        messageId: messageId,
+        seqNo: {$gt: seqNo}
+    }).sort({
+        "seqNo": -1
+    }).exec();
+}
+
+/**
+ * find rumors that are listed as the given node, and whose originators are not themselves.
+ * Also, sort by message id ascending and sequence number descending.
+ *
+ * @param nodeId the node to get the rumors for.
+ * @return {Promise} a promise from the database <code>exec()</code>
+ */
+function getNodeRumors(nodeId) {
+    return Rumor.find({
+        nodeId: nodeId,
+        originator: {$ne: nodeId}
+    }).sort({
+        "messageId": 1,
+        "seqNo": -1
+    }).exec();
+}
+
+/**
+ * Gets rumors between the given nodes
+ * @param first the first node.
+ * @param second the second node.
+ * @return {Promise} a promise from the database <code>exec()</code>
+ */
+function getRumorsBetweenRelationship(first, second) {
+    return Rumor.find({
+        nodeId: first,
+        originator: second
+    }).sort({
+        "messageId": 1,
+        "seqNo": -1
+    }).exec();
+}
+
+router.get('/messages/:nodeId/rumor', function (req, res) {
+    session = req.session;
+    var nodeId = req.params.nodeId;
+    console.log(nodeId);
+    var messageId = req.params.messageId;
+    var seqNo = req.params.seqNo;
+    var originator = req.params.originator;
+    console.log(nodeId);
+    var databasePromise = null;
+    if (messageId !== null && seqNo !== null) {
+        databasePromise = getRumors(nodeId, messageId, seqNo);
+    } else if (originator !== null) {
+        databasePromise = getRumorsBetweenRelationship(nodeId, originator);
+    } else {
+        databasePromise = getNodeRumors(nodeId);
+    }
+    databasePromise
+        .then(function (err, rumors) {
+            res.json(rumors).end();
+        });
 });
 
 router.post('/messages/:nodeId/rumor', function (req, res) {
     session = req.session;
     var nodeId = req.params.nodeId;
+    if (global.processes && global.processes[nodeId]) {
+        var thread = global.processes[nodeId];
+    }
+});
+
+router.get('/messages/:nodeId/want', function (req, res) {
+    session = req.session;
+    var nodeId = req.params.nodeId;
+
 });
 
 router.post('/messages/:nodeId/want', function (req, res) {
     session = req.session;
     var nodeId = req.params.nodeId;
+    console.log(nodeId);
+    if (global.processes && global.processes[nodeId]) {
+        var thread = global.processes[nodeId];
+
+    }
 });
+
+var getUrlForNode = function (req) {
+    return req.protocol + '://' + req.get('host') + req.originalUrl;
+};
 
 /**
  * Spawns a new Gossip node with the given id
  * @param nodeId the id for the node.
+ * @param req the request. Used to the pass the url to the spawned node.
  */
-var spawnNode = function (nodeId) {
+var spawnNode = function (nodeId, req) {
     // Create global processes if it doesn't exist
     if (global.processes === undefined) {
         global.processes = {};
     }
     // Spawn a new node and run it
     var thread = spawn(GossipNode);
-    thread.send({nodeId: nodeId});
+    thread.send({nodeId: nodeId, url: getUrlForNode(req)});
     global.processes[nodeId] = thread;
 };
 
@@ -56,20 +148,22 @@ router.post('/nodes/:nodeId', function (req, res) {
     session = req.session;
     var nodeId = req.params.nodeId;
     console.log("Creating node with Id" + nodeId);
-    spawnNode(nodeId);
+    spawnNode(nodeId, req);
     res.status(200).end();
 });
 
 router.delete('/nodes/:nodeId', function (req, res) {
     session = req.session;
     var nodeId = req.params.nodeId;
+    console.log("Deleting node: " + nodeId);
     if (global.processes && global.processes[nodeId]) {
         var thread = global.processes[nodeId];
         thread.kill();
+        delete global.processes[nodeId];
         res.status(200);
     }
     else {
-        res.status(404).send(nodeId + " not found. Couldn't stop.")
+        res.status(404).send(nodeId + " not found. Couldn't stop.");
     }
     res.end();
 });
@@ -80,14 +174,15 @@ router.delete('/nodes/:nodeId', function (req, res) {
 router.get('/peers/:nodeId', function (req, res) {
     session = req.session;
     var nodeId = req.params.nodeId;
+    console.log("Getting Peers for " + nodeId);
     Relationship.find({nodeId: nodeId}, function (err, result) {
-        var peers = [];
         if (err) {
             res.status(400).send(nodeId + "is not a valid node, or there was an error.");
         }
         else if (result && result.length) {
-            result.forEach(function (friend, index) {
-                peers.push(friend);
+            var peers = [];
+            result.forEach(function (peer, index) {
+                peers.push(peer);
             });
             res.json(peers);
         }
